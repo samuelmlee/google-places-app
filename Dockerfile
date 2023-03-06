b60633b886eddfa31f57906c76678333bdad5909
Sam <sam.leefr@gmail.com>
2023-03-06 23:25:48 +0100

    separate server

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:18-slim
-WORKDIR ./
-COPY ./ dist/private-google-places
-RUN npm install -g @angular/cli
-RUN npm install
-RUN npm run build--prod
-EXPOSE 4200
-CMD ["node", "server.js"]
--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import proxy from "express-http-proxy";
-
-const app = express();
-app.use("/google", proxy("https://maps.googleapis.com"));
-
-app.use(express.static("dist/private-google-places"));
-
-app.listen(4200);
